c5e065e45bf42f14cc0c69cc1fe11c4996e0d7e5
raisyaf20 <raisyafmuhamad@gmail.com>
2024-01-31 11:16:36 +0700

    modify ui auth and added navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,9 +19,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
-          <Navbar />
           <p>Get started by {user?.fullname}</p>
           <div>
             <a
